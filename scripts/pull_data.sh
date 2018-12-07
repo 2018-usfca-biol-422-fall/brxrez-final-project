0ee041517f7f6603bd0bfd5c23b8e0923f93d898
Brian Rezende <brezende@dons.usfca.edu>
2018-12-06 17:19:55 -0800

    Add quotation marks according to Travis build failure

--- scripts/pull_data.sh
+++ scripts/pull_data.sh
@@ -10,7 +10,7 @@
 
 # We are cutting out the 9th column because it contains the SRA numbers that
 # fastq dump needs to download
-for SRA_number in $(cut -f 9 data/metadata/SraRunTable.txt)
+for SRA_number in "$(cut -f 9 data/metadata/SraRunTable.txt)"
 do 
   # We use the full path because fastq dump is not installed in our normal path
   /Users/brezende2015/sratoolkit.2.9.2-mac64/bin/fastq-dump.2.9.2 -v \
